7409b07f3c186be8e6385038e53719fc73ed92ac
mfrizin <mfrizin@sanador.ru>
2024-07-29 10:43:02 +0300

    01,02

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,18 @@
-import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+import {createApp, defineComponent} from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup(){
+    const date = new Date();
+
+    const formatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' })
+
+    const formattedDate = formatter.format(date);
+
+    return { formattedDate };
+  },
+
+  template: `<div>Сегодня {{ formattedDate }}</div>`,
+})
+createApp(App).mount('#app')
--- 01-basics/20-weather/WeatherApp.js
+++ 01-basics/20-weather/WeatherApp.js
@@ -1,51 +1,73 @@
+// import { defineComponent } from 'vue/dist/vue.esm-bundler.js'
 import { defineComponent } from 'vue/dist/vue.esm-bundler.js'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
 
 export default defineComponent({
   name: 'WeatherApp',
 
+  setup() {
+    const weatherData = getWeatherData();
+
+    const getWeatherIcon = (index) => {
+      return WeatherConditionIcons[weatherData[index].current.weather.id];
+    }
+
+    const nowSunStatus = (index) => {
+      if ( weatherData[index].current.dt < weatherData[index].current.sunrise
+        && weatherData[index].current.dt < weatherData[index].current.sunset){
+        return "weather-card--night"
+      }
+      return null;
+    }
+
+    const weatherTemp = (index) => {
+      return (weatherData[index].current.temp - 273.15).toFixed(1);
+    }
+
+    return {weatherData, getWeatherIcon, weatherTemp, nowSunStatus}
+  },
+
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
+        <li v-for="(weather, index) in weatherData" :key="index" class="weather-card" :class="nowSunStatus(index)">
+          <div v-if="weather.alert !== null" class="weather-alert">
             <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
+            <span class="weather-alert__description">{{ weather.alert.sender_name + weather.alert.description }}</span>
           </div>
           <div>
             <h2 class="weather-card__name">
-              Гондор
+              {{weather.geographic_name}}
             </h2>
             <div class="weather-card__time">
-              07:17
+              {{weather.current.dt}}
             </div>
           </div>
           <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
+            <div class="weather-conditions__icon" :title="weather.current.weather.description">{{ getWeatherIcon(index) }}</div>
+            <div class="weather-conditions__temp">{{ weatherTemp(index) + " °C" }}</div>
           </div>
           <div class="weather-details">
             <div class="weather-details__item">
               <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
+              <div class="weather-details__item-value">{{ Math.round(weather.current.pressure * 0.75) }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
+              <div class="weather-details__item-value">{{ weather.current.humidity }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
+              <div class="weather-details__item-value">{{ weather.current.clouds }}</div>
             </div>
             <div class="weather-details__item">
               <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
+              <div class="weather-details__item-value">{{ weather.current.wind_speed }}</div>
             </div>
           </div>
         </li>
       </ul>
     </div>
-  `,
-})
+    `})
